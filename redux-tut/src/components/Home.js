bf76994670051fada7d20cb9af7d7c83f6ceea13
Kajal Pandey <119345768+sp8172975@users.noreply.github.com>
2024-01-23 09:27:24 +0530

    first commit

---
+++ redux-tut/src/components/Home.js
@@ -0,0 +1,17 @@
+import React from 'react'
+
+function Home()
+{
+    return(
+        <div>
+            <h1>Home Component</h1>
+            <div className='cart-wrapper'>
+                <div className='img-wrapper item'>
+                <img src="https://www.fdfproject.com/wp-content/uploads/2018/12/iphone-png.png" alt='' />
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Home;
